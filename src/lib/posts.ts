b3996dda7f68cc7a701ca6b58146bb3362ada10c
davicbtoliveira <davicbtoliveira@users.noreply.github.com>
2026-07-29 14:36:45 -0400

    chore: removing old archives

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,19 +1,9 @@
 import type { PostEntry } from "../content/schemas";
-import {
-  pickRecentPosts,
-  PICK_RECENT_POSTS_DEFAULT_LIMIT,
-} from "./home";
 import { listPosts, listTags } from "./blog-listing";
 import { getCollectionEntries } from "./collection";
 
-export { pickRecentPosts, PICK_RECENT_POSTS_DEFAULT_LIMIT };
 export { listPosts, listTags };
 export type { PostEntry };
-
-export async function getRecentPostEntries(): Promise<PostEntry[]> {
-  const entries = await getCollectionEntries<PostEntry["data"]>("blog");
-  return pickRecentPosts(entries);
-}
 
 export async function getPostEntries(
   filters: Parameters<typeof listPosts>[1] = {},
--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,20 +1,10 @@
 import type { ProjectEntry } from "../content/schemas";
-import {
-  pickFeaturedProjects,
-  PICK_FEATURED_PROJECTS_DEFAULT_LIMIT,
-} from "./home";
 import { listProjects } from "./project-listing";
 import { enrichProjects, type EnrichedProjectEntry } from "./github-enrichment";
 import { getCollectionEntries } from "./collection";
 
-export { pickFeaturedProjects, PICK_FEATURED_PROJECTS_DEFAULT_LIMIT };
 export { listProjects };
 export type { ProjectEntry };
-
-export async function getFeaturedProjectEntries(): Promise<ProjectEntry[]> {
-  const entries = await getCollectionEntries<ProjectEntry["data"]>("projects");
-  return pickFeaturedProjects(entries);
-}
 
 export async function getProjectEntries(
   filters: Parameters<typeof listProjects>[1] = {},
